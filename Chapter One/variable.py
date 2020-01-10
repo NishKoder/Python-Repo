ed874fb5ef409f04cf007f98455f168792f20d28
Nishant Gupta <nishantgupta.ibees@gmail.com>
2020-01-10 16:11:42 +0530

    \First Chapter

---
+++ Chapter One/variable.py
@@ -0,0 +1,15 @@
+# Define & Initialize Variable
+
+number1 = 3
+print(number1)
+
+number2 = "test"
+number2 = 3.0  # Python is Dynamic Programming Language
+print(number2)
+
+
+# Variable Naming Rules
+# Can't Start with Number
+# Can start with Any Letter or _ only
+# Can't use special chacter except _
+# Use snake case writing (i.e. snake_case_writing)
